b7496cc0abd4471b630f0126c3f4cc07e00fb091
wangyao-i <iwangyao@outlook.com>
2026-04-17 16:10:49 +0800

    mxfp8 eplb适配

--- vllm_ascend/eplb/core/eplb_utils.py
+++ vllm_ascend/eplb/core/eplb_utils.py
@@ -77,7 +77,12 @@ def init_eplb_config(eplb_config, layer_id, moe_config, mix_placement=False, num
     if expert_map_path:
         eplb_enable = True
         global_placement, physical_count = expert_file_to_tensor(expert_map_path, layer_id)
-        n_redundant = physical_count - n_experts
+        if physical_count is not None:
+            n_redundant = physical_count - n_experts
+            # if not moe_config.supports_eplb:
+            #     raise ValueError("Eplb supports only w8a8_dynamic quantization.")
+        else:
+            eplb_enable = False
     elif not eplb_enable:
         _, expert_map, _ = determine_expert_map(ep_size, moe_config.ep_rank, n_experts)
         return None, expert_map, None, 0
