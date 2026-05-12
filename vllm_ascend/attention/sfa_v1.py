4e2fb3b500d753e4cf6c03bbd31a2ced3ce43a39
Li Jiahang <216526138+lijiahang226@users.noreply.github.com>
2026-05-12 09:30:02 +0800

    fix

--- vllm_ascend/attention/sfa_v1.py
+++ vllm_ascend/attention/sfa_v1.py
@@ -976,6 +976,7 @@ class AscendSFAImpl(MLAAttentionImpl):
         actual_seq_lengths_key: torch.Tensor,
     ):
         num_actual_tokens = attn_metadata.num_actual_tokens
+        x = x[:num_actual_tokens]
         cos = cos[:num_actual_tokens]
         sin = sin[:num_actual_tokens]
 
